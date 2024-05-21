d51346256cdfd2716a5173a454b6d4386d97c328
Deepramakhal <deepra313131@gmail.com>
2024-05-21 12:36:27 +0530

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+function SignUpValidation(){
+    var customerName = document.getElementById('customer-name').value;
+    var customerNumber = document.getElementById('customer-number').value;
+    var customerPassword = document.getElementById('customer-password').value;
+    if(customerName == ""){
+        alert()
+    }
+
+}
